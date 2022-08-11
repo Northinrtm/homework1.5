74ab18048dd45d7b5ea652194beca4ffa0708d25
northinrtm <artsdurden@gmail.com>
2022-08-11 21:06:20 +0300

    task4

---
+++ src/Task4.java
@@ -0,0 +1,9 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int[] i = new int[]{1, 2, 3};
+        for(int a = 0; a <= i.length - 1;a++){
+            i[a]+=1;
+            System.out.print(i[a] + " ");
+        }
+    }
+}
