83ad794d4a70845bd0d413d822748148cbe72810
northinrtm <artsdurden@gmail.com>
2022-08-11 21:38:56 +0300

    исправил 4ое задание

--- src/Task4.java
+++ src/Task4.java
@@ -2,8 +2,10 @@ public class Task4 {
     public static void main(String[] args) {
         int[] i = new int[]{1, 2, 3};
         for(int a = 0; a <= i.length - 1;a++){
-            i[a]+=1;
-            {System.out.print(i[a] + " ");}
+            if(i[a] % 2 != 0){ i[a] += 1; }
+            }
+        for(int n = 0; n <= i.length - 1;n++){
+            System.out.print(i[n] + " ");
         }
     }
 }
