4086af0f4aab602ab85b99e3408b0010a3d24690
northinrtm <artsdurden@gmail.com>
2022-08-11 20:42:52 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] i = new int[]{1, 2, 3};
+        float[] f = new float[]{1.57f, 7.654f, 9.986f};
+        String[] s = {"qwe", "rty", "yui"};
     }
 }
