3678f9a443a11358c671f22ccac48fc90ba64605
northinrtm <artsdurden@gmail.com>
2022-08-11 21:25:01 +0300

    исправил 1 задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] i = new int[]{1, 2, 3};
+        int[] i = new int[3];
+        i[0] = 1;
+        i[1] = 2;
+        i[2] = 3;
         float[] f = new float[]{1.57f, 7.654f, 9.986f};
         {String[] s = {"qwe", "rty", "yui"};
         for(int a = 0; a <= i.length - 1;a++){
