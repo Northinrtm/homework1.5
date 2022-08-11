084261a5e21bcc99e6d8e82f8bda491a8b677318
northinrtm <artsdurden@gmail.com>
2022-08-11 21:01:31 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int[] i = new int[]{1, 2, 3};
         float[] f = new float[]{1.57f, 7.654f, 9.986f};
-        String[] s = {"qwe", "rty", "yui"};
+        {String[] s = {"qwe", "rty", "yui"};
         for(int a = 0; a <= i.length - 1;a++){
             System.out.print(i[a]);
             if(a != i.length - 1) System.out.print(", ");
@@ -16,4 +16,6 @@ public class Main {
         for(int a = 0; a <= s.length - 1;a++){
             System.out.print(s[a]);
             if(a != i.length - 1) System.out.print(", ");
-}}}
+}
+    }
+    }}
